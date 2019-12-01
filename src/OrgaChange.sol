a1e6647644d8d9c9c83672dd80d02c71ece83003
Tim Keller <55090321+TK5-Tim@users.noreply.github.com>
2019-12-01 10:02:04 +0100

    first version orga change with methods

--- src/OrgaChange.sol
+++ src/OrgaChange.sol
@@ -1,16 +1,62 @@
-pragma solidity ^0.5.11
+pragma solidity 0.4.26;
 
-// Smart Contract for the 2019 Blockchain Challenge at Uni Basel 
-// Case Verein für Vorsorge 
-// Contributors: 
-//  Julian Mordig, Alex Walter und Tim Keller 
+// Smart Contract for the 2019 Blockchain Challenge at Uni Basel
+// Case Verein für Vorsorge
+// Contributors:
+//  Julian Mordig, Alex Walter und Tim Keller
 
 contract OrgaChange {
-   struct TransactionDetails {
-        uint _currentBalance;
-        uint _date;
-        uint _randomNumber;
+   
+   // Declaration Start
+    uint[2] fs_orga;
+    uint[2] pk_orga;
+    uint private orgaFrom;
+    uint orgaTo;
+    uint orgaNow;
+    uint kundeSV;
+    uint balance;
+
+    // Declaration End
+
+
+    // -----------
+
+    //Constructor
+    constructor() public {
+        fs_orga[0] = 0;
+        fs_orga[1] = 1;
+
     }
 
-    
+
+    // Modifiers Start
+
+    // Modifiers End
+
+    // Functions Start
+    function startOrgaChange(uint _orgaFrom, uint _kundeSV, uint _balance) public {
+        orgaFrom = _orgaFrom;
+        orgaNow = orgaFrom;
+        kundeSV = _kundeSV;
+        balance = _balance;
+
+    }
+
+    function getOrga(uint _orgaTo) public {
+        orgaTo = _orgaTo;
+        finishOrgaChange();
+    }
+
+    function finishOrgaChange() private {
+        orgaNow = orgaTo;
+    }
+
+    // Functions End
+
+    // Public Getters Start
+
+    function getCurrentOrga() public view returns (uint) {
+        return orgaNow;
+    }
+    // Public Getters End
 }
