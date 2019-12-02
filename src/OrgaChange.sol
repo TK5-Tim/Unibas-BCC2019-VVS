743ed32b7c13f98671f3f16a51d905db51fbd50e
Tim Keller <55090321+TK5-Tim@users.noreply.github.com>
2019-12-02 22:55:53 +0100

    add address restrictions

--- src/OrgaChange.sol
+++ src/OrgaChange.sol
@@ -10,7 +10,7 @@ contract OrgaChange {
    // Declaration Start
 
    struct orga {
-       uint id;
+       address id;
        string name;
    }
     orga[10] orgas;
@@ -29,23 +29,23 @@ contract OrgaChange {
     constructor() public {
 
         orgas[0].name = "Freizügigkeitsstiftung 1";
-        orgas[1].id = 2;
+        orgas[1].id = 0x2153F81fCFf291eC701A07647F81012ebd8A0f94;
         orgas[1].name = "Freizügigkeitsstiftung 2";
-        orgas[2].id = 3;
+        orgas[2].id = 0xb6bBE3d6C76aE14c6B3A17DEaB98C9b17A612983;
         orgas[2].name = "Freizügigkeitsstiftung 3";
-        orgas[3].id = 4;
+        orgas[3].id = 0x3a51f2Fe69Ae57BF59B8056893c610B8Bd187708;
         orgas[3].name = "Freizügigkeitsstiftung 4";
-        orgas[4].id = 5;
+        orgas[4].id = 0x0E2dDfF7E2aE03F51415C1fFEe20c7ABd8760Fbd;
         orgas[4].name = "Freizügigkeitsstiftung 5";
-        orgas[5].id = 1;
+        orgas[5].id = 0xa3fe2eB0fa8bd67c5B2De91190B6F08554ad03D1;
         orgas[5].name = "Pensionskasse 1";
-        orgas[6].id = 2;
+        orgas[6].id = 0xEab0Ec9de88C6B918Ba18618B39A667acC4C5CE0;
         orgas[6].name = "Pensionskasse 2";
-        orgas[7].id = 3;
+        orgas[7].id = 0x18a3981B30D5A761242BAfcbF523816039D0b375;
         orgas[7].name = "Pensionskasse 3";
-        orgas[8].id = 4;
+        orgas[8].id = 0x46DcE446AFf362349443dDCF2e1400B6c4e4C4EA;
         orgas[8].name = "Pensionskasse 4";
-        orgas[9].id = 5;
+        orgas[9].id = 0xfbCc0103515e776BAdBC292Fb8ca02Df696E6f87;
         orgas[9].name = "Pensionskasse 5";
         orgaChangeStarted = false;
     }
@@ -66,10 +66,20 @@ contract OrgaChange {
         require(orgaIsPart(_orga),"The Organization you choose is not part of the network");
         _;
     }
+
+    modifier OnlyOrgas(){
+        require(addressIsPart(msg.sender),"You are not allowed to trigger that function");
+        _;
+    }
+
+    modifier NoOrgas(){
+        require(!addressIsPart(msg.sender),"You are not allowed to trigger that function");
+        _;
+    }
     // Modifiers End
 
     // Functions Start
-    function startOrgaChange(string _orgaFrom, uint _kundeSV, uint _balance) public OrgaChangeNotActive OrgaInNetwork(_orgaFrom) {
+    function startOrgaChange(string _orgaFrom, uint _kundeSV, uint _balance) public OnlyOrgas OrgaChangeNotActive OrgaInNetwork(_orgaFrom) {
         orgaFrom = _orgaFrom;
         orgaNow = orgaFrom;
         kundeSV = _kundeSV;
@@ -77,7 +87,7 @@ contract OrgaChange {
         orgaChangeStarted = true;
     }
 
-    function getOrga(string _orgaTo) public OrgaChangeInProgress OrgaInNetwork(_orgaTo) {
+    function getOrga(string _orgaTo) public NoOrgas OrgaChangeInProgress OrgaInNetwork(_orgaTo) {
         require(orgaIsPart(_orgaTo),"The Organization you choose is not part of the network");
         if (orgaIsPart(_orgaTo)) {
         orgaTo = _orgaTo;
@@ -96,6 +106,16 @@ contract OrgaChange {
         uint arrayLength = orgas.length;
         for(uint i = 0;i < arrayLength; i++){
             if(keccak256(abi.encodePacked((_orga))) == keccak256(abi.encodePacked((orgas[i].name)))){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function addressIsPart(address _address) public returns (bool) {
+        uint arrayLength = orgas.length;
+        for(uint i = 0;i < arrayLength; i++){
+            if( _address == orgas[i].id){
                 return true;
             }
         }
