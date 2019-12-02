b1354a4210b9d3742c27f69cae70b5b07d865738
Tim Keller <55090321+TK5-Tim@users.noreply.github.com>
2019-12-02 17:26:42 +0100

    implement first capabilities with strings

--- src/OrgaChange.sol
+++ src/OrgaChange.sol
@@ -6,16 +6,21 @@ pragma solidity 0.4.26;
 //  Julian Mordig, Alex Walter und Tim Keller
 
 contract OrgaChange {
-   
+
    // Declaration Start
-    uint[2] fs_orga;
-    uint[2] pk_orga;
-    uint private orgaFrom;
-    uint orgaTo;
-    uint orgaNow;
+
+   struct orga {
+       uint id;
+       string name;
+   }
+    orga[5] fs_orga;
+    orga[5] pk_orga;
+    string orgaFrom;
+    string orgaTo;
+    string orgaNow;
     uint kundeSV;
     uint balance;
-
+    bool orgaChangeStarted;
     // Declaration End
 
 
@@ -23,40 +28,74 @@ contract OrgaChange {
 
     //Constructor
     constructor() public {
-        fs_orga[0] = 0;
-        fs_orga[1] = 1;
-
+        fs_orga[0].id = 1;
+        fs_orga[0].name = "Freizügigkeitsstiftung 1";
+        fs_orga[1].id = 2;
+        fs_orga[1].name = "Freizügigkeitsstiftung 2";
+        fs_orga[2].id = 3;
+        fs_orga[2].name = "Freizügigkeitsstiftung 3";
+        fs_orga[3].id = 4;
+        fs_orga[3].name = "Freizügigkeitsstiftung 4";
+        fs_orga[4].id = 5;
+        fs_orga[5].name = "Freizügigkeitsstiftung 5";
+        pk_orga[0].id = 1;
+        pk_orga[0].name = "Pensionskasse 1";
+        pk_orga[1].id = 2;
+        pk_orga[1].name = "Pensionskasse 2";
+        pk_orga[2].id = 3;
+        pk_orga[2].name = "Pensionskasse 3";
+        pk_orga[3].id = 4;
+        pk_orga[3].name = "Pensionskasse 4";
+        pk_orga[4].id = 5;
+        pk_orga[4].name = "Pensionskasse 5";
+        orgaChangeStarted = false;
     }
 
 
     // Modifiers Start
+    modifier OrgaChangeNotActive() {
+        require(orgaChangeStarted == false, "Organization Change is already in progress!");
+        _;
+    }
 
+    modifier OrgaChangeInProgress() {
+        require(orgaChangeStarted == true, "Organization Change was not started yet!");
+        _;
+    }
     // Modifiers End
 
     // Functions Start
-    function startOrgaChange(uint _orgaFrom, uint _kundeSV, uint _balance) public {
+    function startOrgaChange(string _orgaFrom, uint _kundeSV, uint _balance) public OrgaChangeNotActive {
         orgaFrom = _orgaFrom;
         orgaNow = orgaFrom;
         kundeSV = _kundeSV;
         balance = _balance;
-
+        orgaChangeStarted = true;
     }
 
-    function getOrga(uint _orgaTo) public {
+    function getOrga(string _orgaTo) public OrgaChangeInProgress {
         orgaTo = _orgaTo;
         finishOrgaChange();
     }
 
     function finishOrgaChange() private {
-        orgaNow = orgaTo;
+        if (getID()) {
+            orgaNow = orgaTo;
+        }
+        orgaChangeStarted = false;
     }
-
     // Functions End
 
     // Public Getters Start
 
-    function getCurrentOrga() public view returns (uint) {
+    function getCurrentOrga() public view returns (string) {
         return orgaNow;
     }
     // Public Getters End
+    // Private Getters Start
+    // Dummy-Funktion, welche die zukünftige Funktionalität mit einer externen Identifizierung repräsentiert.
+    function getID() private view returns (bool) {
+        return true;
+    }
+    // Private Getters End
 }
